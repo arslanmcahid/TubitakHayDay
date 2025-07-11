22633ec5babd27b6140358d8380f338f4ee4e549
Mücahid Sezgin ARSLAN <mucahid.arslan@tbtk.gov.tr>
2025-07-11 11:40:14 +0300

    Fifth and Sixth Steps
    Otomatik tohum gelişimi ve çiçek kuruması

--- src/components/Tarla/tarla.tsx
+++ src/components/Tarla/tarla.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Tohum, { HucreStage } from "../tohum/tohum";
 import styles from "./tarla.module.css";
 
@@ -18,6 +18,35 @@ export default function Tarla() {
   const [stages, setStages] = useState<HucreStage[]>(
     Array(TARLA_SIZE * TARLA_SIZE).fill("boş")
   );
+  const [tick, setTick] = useState(0);
+
+ useEffect(() => {
+    const id = setInterval(() => setTick(t => t + 1), 2000);
+    return () => clearInterval(id);
+  }, []);
+
+
+  useEffect(() => {
+    setStages(prev =>
+      prev.map(s => {
+            // state updates can be asenchronous,
+            // so we need to use the previous state
+            //setINterval içinceki closure kapanış yolu eski bir değer tutabilir
+            // with functional form we can access the previous and live state
+            // yeni state önceki state in bir fonksiyonu
+            // React setState API
+        const i = stageOrder.indexOf(s);
+        if (s === "boş" || s === "kurumuş çiçek") return s;
+        if (s === "çiçek") {
+          return tick % 2 === 0 ? s : "kurumuş çiçek";
+        }
+        if (i >= 1 && i < stageOrder.length - 2) {
+          return stageOrder[i + 1];
+        }
+        return s;
+      })
+    );
+  }, [tick]);
 
   const handleClick = (idx: number) => {
     setStages((prev) => {
